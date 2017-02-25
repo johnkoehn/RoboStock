874b4af383441f07160377eeae338aa00f20f3eb
John Koehn <kingkoehn@gmail.com>
2017-02-25 13:03:05 -0600

    merge

---
+++ RoboStock/src/robo/Day.java
@@ -0,0 +1,27 @@
+package robo;
+
+public class Day
+{
+	protected float open;
+	protected float close;
+	Day(float open, float close)
+	{
+		this.open = open;
+		this.close = close;
+	}
+	
+	public float getOpen()
+	{
+		return open;
+	}
+	
+	public float getClose()
+	{
+		return close;
+	}
+	
+	public String toString()
+	{
+		return Float.toString(open) + " : " + Float.toString(close);
+	}
+}
